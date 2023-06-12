9cd656a1b7976be2491defaa0f285712005cf935
Diego Franco Giusti Modesto da Silva <diegogiusti08@gmail.com>
2023-06-12 18:55:21 -0300

    update
    Melhorando algumas partes do código

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
 const formSearch = document.querySelector('.form-search')
+
 let liGroupItem = document.querySelectorAll('li.list-group-item')
 let deleteIcons = document.querySelectorAll('.delete')
 
@@ -9,21 +10,25 @@ const updateNodeLists = () => {
   deleteIcons = document.querySelectorAll('.delete')
 }
 
-const addTodo = inputValue => {
-  const isDifferentFromZero = inputValue.length
+const addTodo = event => {
+  event.preventDefault()
 
-  if(isDifferentFromZero){
-    todoIndex = liGroupItem.length
+  const inputValue = event.target.add.value.trim()
 
-    todosContainer.innerHTML += `
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${inputValue}</span>
-        <i class="far fa-trash-alt delete" data-todo-index="${todoIndex}"></i>
-      </li>
-    `
-    updateNodeLists()
-    formAddTodo.reset()
+  if(inputValue.length < 3 || inputValue === ''){
+    alert('Por vavor, insira um texto valido!')
+    return
   }
+
+  todosContainer.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${inputValue}</span>
+      <i class="far fa-trash-alt delete" data-todo-index="${liGroupItem.length}"></i>
+    </li>
+  `
+
+  updateNodeLists()
+  formAddTodo.reset()
 }
 
 const setElementsAttribute = (element,attribute) => {
@@ -32,7 +37,10 @@ const setElementsAttribute = (element,attribute) => {
   })
 }
 
-const deleteTodo = (getDeleteIcon,clickedElement) => {
+const deleteTodo = event => {
+  const clickedElement = event.target
+  const getDeleteIcon = Array.from(clickedElement.classList).includes('delete')
+
   if(getDeleteIcon){
     const indexTodo = clickedElement.dataset.todoIndex
 
@@ -43,36 +51,18 @@ const deleteTodo = (getDeleteIcon,clickedElement) => {
   }
 }
 
-const setClassList = (element,removeClass,addClass) => {
-  element.classList.remove(removeClass)
-  element.classList.add(addClass)
-}
+const searchTodo = event => {
+  const inputValue = event.target.value.trim().toLowerCase()
+  const todos = Array.from(todosContainer.children)
 
-const searchTodo = (todos,inputValue) => {
   todos.forEach(todo => {
-    let valueIncludedInList = todo.textContent.toLowerCase().includes(inputValue)
+    let shouldBeVisible = todo.textContent.toLowerCase().trim().includes(inputValue)
 
-    !valueIncludedInList ? setClassList(todo,'d-flex','hidden') : setClassList(todo,'hidden','d-flex')
+    todo.classList.add(shouldBeVisible ? 'd-flex' : 'd-none')
+    todo.classList.remove(shouldBeVisible ? 'd-none' : 'd-flex')
   })
 }
 
-formAddTodo.addEventListener('submit', event => {
-  event.preventDefault()
-
-  const inputValue = event.target.add.value.trim()
-  addTodo(inputValue)
-})
-
-todosContainer.addEventListener('click',event => {
-  const clickedElement = event.target
-  const getDeleteIcon = Array.from(clickedElement.classList).includes('delete')
-
-  deleteTodo(getDeleteIcon,clickedElement)
-})
-
-formSearch.addEventListener('input',event => {
-  const inputValue = event.target.value.trim().toLowerCase()
-  const todos = Array.from(todosContainer.children)
-   
-  searchTodo(todos,inputValue)
-})
+formAddTodo.addEventListener('submit', addTodo)
+todosContainer.addEventListener('click', deleteTodo)
+formSearch.addEventListener('input',searchTodo)
